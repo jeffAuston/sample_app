616ff3885d2e684372e993bf0db6757a80980bec
Jeff Auston <jeff.auston@yahoo.com>
2013-08-14 12:54:14 -0700

    updates to pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,32 +3,41 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
-  
-    it "should have the right title" do
+    it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_content('Sample App')
     end
 
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
   end
 
-    describe "Help page" do
-
+  describe "Help page" do
       it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | HelpMe")
     end
-
-   
   end
 
-   describe "About page" do
-
+  describe "About page" do
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | AboutUs")
     end
-
-    
   end
-  
+
+  describe "Contact" do
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
